e314124cf192f43e7d9d23c2a01b8c9c105c45a0
zhoube <zhouyikelvin@gmail.com>
2026-02-24 16:50:53 +0800

    Initial commit

---
+++ src/main/java/com/example/interviewproductassignment/InterviewProductAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewproductassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewProductAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewProductAssignmentApplication.class, args);
+    }
+
+}
