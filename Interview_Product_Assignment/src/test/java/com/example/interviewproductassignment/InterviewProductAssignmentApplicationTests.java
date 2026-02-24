562229aac38e810975e9e25dc7b457fc176e9e3e
zhoube <zhouyikelvin@gmail.com>
2026-02-24 16:48:26 +0800

    Initial Spring Boot project setup

---
+++ Interview_Product_Assignment/src/test/java/com/example/interviewproductassignment/InterviewProductAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewproductassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewProductAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
